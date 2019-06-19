a23ea1d5dc8c5376573aeb27b1cbb2041a9fdda3
michael.brewster <michael.brewster@ibm.com>
2019-06-19 07:19:22 +0100

    Complete Section16

--- Section16/myDjangoEnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/michaelbrewster/Desktop/myGit/djangoProject/Section16/myDjangoEnv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
