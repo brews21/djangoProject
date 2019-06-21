306b43b252818f0366a5e55811a6cc93d8f2a03c
michael.brewster <michael.brewster@ibm.com>
2019-06-21 09:20:32 +0100

    Complete section20

---
+++ Section20/first_project/first_app/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from django.urls import path
+from first_app import views
+
+# Template tagging
+app_name = 'first_app'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('users/', views.users, name='users'),
+    path('formpage/', views.form_name_view, name='form_page'),
+    path('other/', views.other, name='other'),
+    path('relative_url_templates/', views.relative_url_templates, name='relative_url_templates'),
+    path('registration/', views.registration, name='registration'),
+    path('login/', views.user_login, name='user_login'),
+]
